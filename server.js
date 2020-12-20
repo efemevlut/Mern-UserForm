bcab08b77e30e49fea5dc5aa3e96ecaf2e7a800e
efemevlut <efemevlut20@gmail.com>
2020-12-20 16:26:04 +0300

    Connect database

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const dbConnect = require("./models/dbConnect");
 require("dotenv").config();
 
 
 //server
 const app = express();
 const port = process.env.port || 5000;
-
 app.listen(port, ()=>{
     console.log(`I am listening on port ${port}`)
 });
+
+//database connect
+dbConnect();
 
 app.use(express.json());
 // app.use("/api", router);
