3899ae34f992656aef2dcf5bfa636e37a785951b
efemevlut <efemevlut20@gmail.com>
2020-12-24 19:51:40 +0300

    Remove client from heroku

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+  app.use("/api", router)
 }
 
 app.use(
