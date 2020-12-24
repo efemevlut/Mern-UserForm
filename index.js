96d96c179388206bac1ac947dbf4cda7a38b6734
efemevlut <efemevlut20@gmail.com>
2020-12-24 18:35:15 +0300

    Add production case

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ require("dotenv").config();
 const router = require("./routers/router");
 const cors = require("cors");
 
-
 //server
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`I am listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`I am listening on port ${port}`);
 });
 
 //database connect
 dbConnect();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
+//production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
     credentials: true,
-  }));
+  })
+);
 app.use(express.json());
 app.use("/api", router);
