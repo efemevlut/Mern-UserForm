c500de99bff0b6248d16d96163d8c5713f868660
efemevlut <efemevlut20@gmail.com>
2020-12-25 12:46:33 +0300

    Add production env

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.listen(port, () => {
 //database connect
 dbConnect();
 
-//production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-//   app.use("/api", router)
-// }
+production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  app.use("/api", router)
+}
 
 // app.use(
 //   cors({
